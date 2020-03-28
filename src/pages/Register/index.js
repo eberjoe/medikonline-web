592878c099b2624b91a5e6a19e5ddde5564d9aec
eberjoe <eberjoe@gmail.com>
2020-03-28 17:40:47 -0300

    Add registration page ans styles

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -13,13 +13,22 @@ export default function Register() {
                     <img src={logoImg} alt="Be The Hero"/>
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
-                    <Link className="back-link" to="/register">
+                    <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#e02041" />
-                        Não tenho cadastro
+                        Voltar para logon
                     </Link>
                 </section>
                 <form>
-
+                    <input placeholder="Nome da ONG"/>
+                    <input type="email" placeholder="E-mail" />
+                    <input placeholder="WhatsApp"/>
+                    <div className="input-group">
+                        <input placeholder="Cidade" />
+                        <input placeholder="UF" style={{ width: 80 }} />
+                    </div>
+                    <button className="button" type="submit">
+                        Cadastrar
+                    </button>
                 </form>
             </div>
         </div>
