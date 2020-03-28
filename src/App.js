d957394f00ae8c05c38703cac577eccfa9403698
eberjoe <eberjoe@gmail.com>
2020-03-28 10:02:22 -0300

    Add assets, font, and global styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import './global.css';
+import Logon from './pages/Logon';
 
 function App() {
-  // Array [value, function]
-  let [counter, setCounter] = useState(0);
-
-  function increment(){
-    setCounter (counter + 1);
-    console.log(counter);
-  }
   return (
-    <div>
-      <Header>
-        Counter: {counter}
-      </Header>
-      <button onClick={increment}>
-        Incrementar
-      </button>
-    </div>
+    <Logon />
   );
 }
 
