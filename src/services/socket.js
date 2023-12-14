b86dcc2d8602d7403b8e83f7f28258653636fe63
Eber Martins Rodrigues <eberjoe@gmail.com>
2023-12-14 17:12:01 -0300

    Remove port info

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from 'socket.io-client';
 
-const socket = socketIOClient(`${process.env.REACT_APP_SERVER_URL}:3333`);
+const socket = socketIOClient(`${process.env.REACT_APP_SERVER_URL}`);
 
 export default socket;
